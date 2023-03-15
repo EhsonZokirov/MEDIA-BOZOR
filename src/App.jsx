2c15457404a08e3a242e517ffb1cee2ca1d48185
Ehson Zokirov <zokirov_2000@mail.ru>
2023-03-15 18:02:46 +0500

    babki

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import { Button } from "@mui/material";
 import React from "react";
 import logo from "./img/logo.png";
+import CampaignIcon from "@mui/icons-material/Campaign";
 function App() {
   return (
-    <div className="container mx-auto">
-      <div className="my-40">
+    <div className="">
+      <nav className="flex bg-[#CC0909] font-bold text-white p-5 justify-center list-none gap-28">
+        <li className="hover:text-slate-800 cursor-pointer transition">
+          БЛОГЕРЫ
+        </li>
+        <li className="hover:text-slate-800 cursor-pointer transition">
+          КЛИЕНТЫ
+        </li>
+        <li className="hover:text-slate-800 cursor-pointer transition">
+          ПАРТНЁРЫ
+        </li>
+        <li className="hover:text-slate-800 cursor-pointer transition">
+          КОНТАКТЫ
+        </li>
+      </nav>
+      {/*  */}
+      <section className="container mx-auto md:py-[5.99%] bg-slate-100">
         <div className=" flex items-center justify-center">
           <img className=" w-[15%]" src={logo} alt="logo" />
           <div className="font-bold text-[#CC0909] text-5xl">
             MEDIA <br /> BOZOR
           </div>
         </div>
-        <div className="w-[60%] m-auto text-center my-[40px]">
+        <div className="w-[60%] m-auto text-center mt-[40px]">
           <h1 className="font-bold text-5xl ">
             Увлеченная команда видеоблогеров и профессионалов рекламы
           </h1>
@@ -19,8 +36,33 @@ function App() {
             достижения наилучшего результата. Наши проекты повышают лояльность к
             бренду и увеличивают продажи.
           </p>
+          <Button
+            variant="contained"
+            color="error"
+            sx={{ width: "25%" }}
+            className="animate-pulse"
+          >
+            ТОП
+          </Button>
         </div>
-      </div>
+      </section>
+      {/*  */}
+      <section>
+        <h1 className="text-center font-bold text-4xl my-10">
+          Работая с нами, вы получаете:
+        </h1>
+        <div className="grid grid-cols-3 text-center w-[70%] m-auto gap-60">
+          <div>
+            <div>
+              <CampaignIcon />
+            </div>
+            <p>
+              Возможность привлечь в рекламную кампанию{" "}
+              <span className="font-bold">любого блогера Таджнета</span>
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
