d4fe75ba18986938d7b99da4694d7f95f70ab8b3
Ehson Zokirov <zokirov_2000@mail.ru>
2023-03-16 10:55:38 +0500

    add cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,18 @@ import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import SpatialAudioOffIcon from "@mui/icons-material/SpatialAudioOff";
 import MicIcon from "@mui/icons-material/Mic";
+import fozilov from "./img/Fozilov.PNG";
+import nasiba from "./img/Nasiba.JPG";
+import ZANJABIL from "./img/ZANJABIL.JPG";
+import Jonon from "./img/JONON.PNG";
+import azizRahima from "./img/AZIZ & Rahima.PNG";
+import zarka from "./img/ZARKA.PNG";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 function App() {
   return (
     <div className="">
-      <nav className="flex bg-[#CC0909] font-bold text-white p-5 justify-center list-none gap-28">
+      <nav className="flex bg-[#CC0909] sm:font-bold text-white p-5 justify-center list-none gap-1 xs:gap-10 sm:gap-20 md:gap-28">
         <li className="hover:text-slate-800 cursor-pointer transition">
           БЛОГЕРЫ
         </li>
@@ -25,18 +33,18 @@ function App() {
         </li>
       </nav>
       {/*  */}
-      <section className="container mx-auto md:py-[5.99%] bg-slate-100">
+      <section className="container  mx-auto md:py-[5.99%] bg-slate-100">
         <div className=" flex items-center justify-center">
           <img className=" w-[15%]" src={logo} alt="logo" />
-          <div className="font-bold text-[#CC0909] text-5xl">
+          <div className="font-bold text-[#CC0909] md:text-5xl sm:text-3xl xs:text-xs text-[15px]">
             MEDIA <br /> BOZOR
           </div>
         </div>
         <div className="w-[60%] m-auto text-center mt-[40px]">
-          <h1 className="font-bold text-5xl ">
+          <h1 className="font-bold md:text-5xl sm:text-3xl xs:text-md ">
             Увлеченная команда видеоблогеров и профессионалов рекламы
           </h1>
-          <p className=" my-10">
+          <p className="text-[10px]  md:text-[20px] my-10">
             Мы реализуем рекламные проекты у блогеров, создавая решения для
             достижения наилучшего результата. Наши проекты повышают лояльность к
             бренду и увеличивают продажи.
@@ -53,11 +61,11 @@ function App() {
       </section>
       {/*  */}
       <section className="my-16">
-        <h1 className="text-center font-bold text-4xl my-20">
+        <h1 className="text-center font-bold text-2xl md:text-4xl my-20">
           Работая с нами, вы получаете:
         </h1>
-        <div className="grid grid-cols-3 text-center w-[70%] m-auto gap-28 my-10">
-          <div>
+        <div className="slide-top grid sm:grid-cols-2 lg:grid-cols-3 text-center w-[70%] m-auto gap-28 my-10">
+          <div className="slide-top">
             <div>
               <CampaignIcon color="error" sx={{ fontSize: "100px" }} />
             </div>
@@ -67,7 +75,7 @@ function App() {
               крупные компании и известные бренды
             </p>
           </div>
-          <div>
+          <div className="slide-top">
             <div>
               <WorkHistoryIcon color="error" sx={{ fontSize: "100px" }} />
             </div>
@@ -77,7 +85,7 @@ function App() {
               за те деньги, которые готовы отдать
             </p>
           </div>
-          <div>
+          <div className="slide-top">
             <div>
               <VolunteerActivismIcon color="error" sx={{ fontSize: "100px" }} />
             </div>
@@ -87,7 +95,7 @@ function App() {
               блогеров
             </p>
           </div>
-          <div>
+          <div className="slide-top">
             <div>
               <ThumbUpIcon color="error" sx={{ fontSize: "100px" }} />
             </div>
@@ -97,7 +105,7 @@ function App() {
             </p>
           </div>
           <div>
-            <div>
+            <div className="slide-top">
               <SpatialAudioOffIcon color="error" sx={{ fontSize: "100px" }} />
             </div>
             <p className="text-lg">
@@ -106,7 +114,7 @@ function App() {
               под ваш продукт
             </p>
           </div>
-          <div>
+          <div className="slide-top">
             <div>
               <MicIcon color="error" sx={{ fontSize: "100px" }} />
             </div>
@@ -114,6 +122,84 @@ function App() {
               Возможность привлечь в рекламную кампанию
               <span className="font-bold"> любого блогера Таджнета</span>
             </p>
+          </div>
+        </div>
+      </section>
+      {/*  */}
+      <section className=" bg-slate-100 py-10">
+        <h1 className="text-center font-bold text-4xl mb-10"> БЛОГЕРЫ </h1>
+        <div className="grid text-[#CC0909] xs:grid-cols-2 lg:grid-cols-3 m-auto text-center font-medium md:w-[80%] px-5 gap-5">
+          <div className="m-auto cursor-pointer border bg-white md:w-[50%] shadow-md hover:shadow-lg transition-all duration-300">
+            <h1>Ilhomjon</h1>
+            <img className=" m-auto" src={fozilov} alt="Fozilov IMG" />
+            <div className="p-2 flex  justify-between px-5">
+              <div>
+                <InstagramIcon /> 250k
+              </div>
+              <div>
+                <LibraryMusicIcon /> 150k
+              </div>
+            </div>
+          </div>
+          <div className="m-auto items-center cursor-pointer border bg-white md:w-[50%] shadow-md hover:shadow-lg transition-all duration-300">
+            <h1>Nasiba</h1>
+            <img className=" m-auto" src={nasiba} alt="Fozilov IMG" />
+            <div className="p-2 flex  justify-between px-5">
+              <div>
+                <InstagramIcon /> 250k
+              </div>
+              <div>
+                <LibraryMusicIcon /> 150k
+              </div>
+            </div>
+          </div>
+          <div className="m-auto items-center cursor-pointer border bg-white md:w-[50%] shadow-md hover:shadow-lg transition-all duration-300">
+            <h1>ZanjabilBro</h1>
+            <img className=" m-auto" src={ZANJABIL} alt="Fozilov IMG" />
+            <div className="p-2 flex  justify-between px-5">
+              <div>
+                <InstagramIcon /> 250k
+              </div>
+              <div>
+                <LibraryMusicIcon /> 150k
+              </div>
+            </div>
+          </div>
+          <div className="m-auto items-center cursor-pointer border bg-white md:w-[50%] shadow-md hover:shadow-lg transition-all duration-300">
+            <h1>Jonon</h1>
+            <img className=" m-auto" src={Jonon} alt="Fozilov IMG" />
+            <div className="p-2 flex  justify-between px-5">
+              <div>
+                <InstagramIcon /> 250k
+              </div>
+              <div>
+                <LibraryMusicIcon /> 150k
+              </div>
+            </div>
+          </div>
+          <div className="m-auto items-center cursor-pointer border bg-white md:w-[50%] shadow-md hover:shadow-lg transition-all duration-300">
+            <h1>Aziz & Rahima</h1>
+            <img className=" m-auto" src={azizRahima} alt="Fozilov IMG" />
+            <div className="p-2 flex  justify-between px-5">
+              <div>
+                <InstagramIcon /> 250k
+              </div>
+              <div>
+                <LibraryMusicIcon /> 150k
+              </div>
+            </div>
+          </div>
+          <div className="m-auto items-center cursor-pointer border bg-white md:w-[50%] shadow-md hover:shadow-lg transition-all duration-300">
+            <h1>Zarka</h1>
+            <img className=" m-auto" src={zarka} alt="Fozilov IMG" />
+            <div className="p-2 flex justify-between px-5">
+              <div>
+                <InstagramIcon /> 250k
+              </div>
+              <div>
+                <LibraryMusicIcon /> 150k
+              </div>
+            </div>
           </div>
         </div>
       </section>
