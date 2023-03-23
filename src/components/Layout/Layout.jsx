8235004b4a5c0b95c31f48a6f83492b0f68e2786
Ehson Zokirov <zokirov_2000@mail.ru>
2023-03-23 20:05:45 +0500

    removed empty line

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -11,10 +11,7 @@ import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 function Layout() {
   const [state, setState] = useState({
     top: false,
