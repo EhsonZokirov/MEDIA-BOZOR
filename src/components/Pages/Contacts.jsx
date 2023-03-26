8b3db7b3306c15b6779b6f03f55099daa0b82a4a
Ehson Zokirov <zokirov_2000@mail.ru>
2023-03-26 13:42:57 +0500

    edit 404

--- src/components/Pages/Contacts.jsx
+++ src/components/Pages/Contacts.jsx
@@ -1,6 +1,5 @@
 import { Button, TextField } from "@mui/material";
 import React from "react";
-import { NavLink } from "react-router-dom";
 // import logo from "../img/logo.png";
 function Contacts() {
   return (
@@ -9,21 +8,20 @@ function Contacts() {
       {/* section */}
       <section className="py-[143px]">
         <form className="grid m-auto px-10 md:w-[40%]">
-          <h1 className=" font-bold text-lg ">Напишите нам:</h1>
+          <h1 className=" font-mono text-lg text-center ">Напишите нам:</h1>
           <TextField required variant="standard" label="Имя" />
           <TextField required variant="standard" label=" Телефон" type="tel" />
           <TextField required variant="standard" label=" Эл. Адрес" />
           <TextField required variant="filled" label="Сообщение" multiline />
-          <NavLink to="Contacts">
-            <Button
-              fullWidth
-              type="submit"
-              variant="contained"
-              sx={{ bgcolor: "purple" }}
-            >
-              Отправить
-            </Button>
-          </NavLink>
+
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            sx={{ bgcolor: "purple" }}
+          >
+            Отправить
+          </Button>
         </form>
       </section>
 
